e03ed9b690865d7b2d079fffbb2bc7e8f64fd9a8
양태웅 <didxodnd0422@gmail.com>
2021-11-28 19:58:35 +0900

    Update BASE_URL

--- SimpleBoardApp/app/src/main/java/com/example/simpleboardapp/util/Constants.kt
+++ SimpleBoardApp/app/src/main/java/com/example/simpleboardapp/util/Constants.kt
@@ -2,6 +2,8 @@ package com.example.simpleboardapp.util
 
 class Constants {
     companion object {
-        const val BASE_URL = "url"
+        const val BASE_URL = "http://10.0.2.2:3000"
+
+        const val TAG = "Patrick0422"
     }
 }
