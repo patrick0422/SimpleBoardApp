32f44acad8843d70b4c8f1b5b2f327dec1ae63bf
양태웅 <didxodnd0422@gmail.com>
2021-11-28 19:58:12 +0900

    Add BaseFragments

---
+++ SimpleBoardApp/app/src/main/java/com/example/simpleboardapp/util/BaseFragment.kt
@@ -0,0 +1,35 @@
+package com.example.simpleboardapp.util
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<B : ViewDataBinding>(
+    @LayoutRes val layoutId: Int
+) : Fragment() {
+    lateinit var binding: B
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        init()
+
+        return binding.root
+    }
+
+    abstract fun init()
+
+    protected fun showToast(msg: String) =
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+}
