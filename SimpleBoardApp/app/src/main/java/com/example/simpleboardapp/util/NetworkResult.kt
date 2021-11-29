3fcd204abf9239755590e7a1869a56574df060a1
양태웅 <didxodnd0422@gmail.com>
2021-11-29 14:32:09 +0900

    Implemennt NetworkResult class

--- SimpleBoardApp/app/src/main/java/com/example/simpleboardapp/util/NetworkResult.kt
+++ SimpleBoardApp/app/src/main/java/com/example/simpleboardapp/util/NetworkResult.kt
@@ -1,4 +1,14 @@
 package com.example.simpleboardapp.util
 
-class NetworkResult {
+sealed class NetworkResult<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+
+    class Success<T>(data: T): NetworkResult<T>(data)
+
+    class Error<T>(message: String?, data: T? = null): NetworkResult<T>(data, message)
+
+    class Loading<T>: NetworkResult<T>()
+
 }
