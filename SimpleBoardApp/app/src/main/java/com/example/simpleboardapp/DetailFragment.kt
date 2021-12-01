e2b44b78f13dc98f508cdc3bd5b03e42a368ff9c
양태웅 <didxodnd0422@gmail.com>
2021-12-01 20:19:54 +0900

    Add UploadFragment and DetailFragment

---
+++ SimpleBoardApp/app/src/main/java/com/example/simpleboardapp/DetailFragment.kt
@@ -0,0 +1,16 @@
+package com.example.simpleboardapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.simpleboardapp.databinding.FragmentDetailBinding
+import com.example.simpleboardapp.util.BaseFragment
+
+class DetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.fragment_detail) {
+
+    override fun init() {
+
+    }
+}
